ededbc64304f373b9b694d40ca66a5e3c8635c91
saugatpokhrel7 <124776103+saugatpokhrel7@users.noreply.github.com>
2023-08-24 14:14:02 +0000

    initial

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+#include <windows.h>
+
+int main() {
+    // To Do
+    // Main objective : Create a simple Excel clone with CRUD operations
+    // Use headers file for functions
+    return 0;
+}
